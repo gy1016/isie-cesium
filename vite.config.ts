ead312b90d4aa10aa22b6bf5373d6e71908972a4
gy <1838940294@qq.com>
2022-02-27 15:55:13 +0800

    chore: 修改构建公共路径并开启gzip压缩

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
+import gzip from 'vite-plugin-compression';
 import cesium from 'vite-plugin-cesium';
 import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), cesium()],
+  plugins: [vue(), cesium(), gzip()],
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src'),
